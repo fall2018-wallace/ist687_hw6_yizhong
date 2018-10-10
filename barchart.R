e8023367f8ead794942844dcdfeff133dd820dc1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 22:55:44 -0400

    Saving barchart.R

--- barchart.R
+++ barchart.R
@@ -3,3 +3,4 @@ library(ggplot2)
 merge_df$murdernum<-merge_df$population/100000*merge_df$Murder
 bar_murdernum<-ggplot(merge_df,aes(x=stateName,y=murdernum))+geom_col()
 bar_murdernum1<-bar_murdernum+theme(axis.text.x = element_text(angle = 90,hjust = 1))+ggtitle("barchart of murderNum")
+bar_murdernum2<-ggplot(merge_df,aes(x=reorder(stateName,murdernum),y=murdernum,group=1))+geom_col()+theme(axis.text.x = element_text(angle = 90,hjust = 1))+ggtitle("barplot of murderNum")
